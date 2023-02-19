cdafed08f16d4228ef01699fcf61a57dadb9a04f
EdgardOliveira <edgard.oliveira@protonmail.com>
2023-02-19 19:09:17 -0400

    feat: Implementação de context api para o Sidebar menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { BaseLayout } from "@/shared/components/baseLayout/BaseLayout";
 export default function Home() {
   return (
     <BaseLayout title="Teste" subtitle="testando 123">
-      <>teste</>
+      <></>
     </BaseLayout>
   );
 }
